7ea892a8147496c7cc38fdc1084a974dafed473d
WardaFarhan <farhan.warda337@gmail.com>
2020-06-17 17:11:40 +0530

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+<script>
+    $(document).ready(function(){
+        $("#mycarousel").carousel( { interval: 1000 } );
+        $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+      //  $("#mycarousel").carousel( { interval: 2000 } );
+        //$("#carousel-pause").click(function(){
+          //$("#mycarousel").carousel('pause');
+        //});
+
+       // $("#carousel-play").click(function(){
+            //$("#mycarousel").carousel('cycle');
+       // });
+    });
+</script>
+//"start": "npm run lite", frm package.json
